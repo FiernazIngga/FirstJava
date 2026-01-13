5ac2546d1b9306cedc56a605a366aac40aeeebee
FiernazIngga <inggapratama86@gmail.com>
2026-01-13 10:02:57 +0700

    secondCommit

---
+++ src/firstjava/inputJava.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package firstjava;
+
+/**
+ *
+ * @author M S I
+ */
+public class inputJava {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
